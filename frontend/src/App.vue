d7be799e4bde8fa5cf0cf456812f594b2b2c7ff9
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-10-31 21:22:29 -0300

    Add basic button component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1>Salut, France !</h1>
+  <Button text="Teste" backgroundColor="#ff0000" height="50px" maxWidth="400px" />
 </template>
 
 <script>
+// components
+import Button from "./components/Button.vue";
 export default {
   name: "App",
+  components: {
+    Button,
+  },
 };
 </script>
 
