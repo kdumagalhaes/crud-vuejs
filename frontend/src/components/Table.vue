78e71c6e982ba566e781b6a6b035c8e47e6e8c7c
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-10-31 23:12:35 -0300

    Add table

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -7,6 +7,7 @@
       <th>Twitter</th>
       <th>Localização</th>
       <th>Último artigo</th>
+      <th>Ações</th>
     </tr>
     <tr v-for="author in authors" :key="author.id">
       <td contenteditable="true">{{ author.name }}</td>
@@ -21,17 +22,36 @@
       <td contenteditable="true">
         {{ author.latest_article_published }}
       </td>
+      <td class="action-column">
+        <Button
+          text="Editar"
+          backgroundColor="#b8a200"
+          height="50px"
+          maxWidth="85px"
+        />
+        <Button
+          text="Apagar"
+          backgroundColor="#ff0000"
+          height="50px"
+          maxWidth="85px"
+        />
+      </td>
     </tr>
   </table>
 </template>
 
 <script>
+// components
+import Button from "./Button.vue";
 export default {
   name: "Table",
   data() {
     return {
       authors: "",
     };
+  },
+  components: {
+    Button,
   },
   methods: {
     async listAllAuthors() {
@@ -56,18 +76,29 @@ export default {
   width: 100%;
   max-width: 1200px;
   margin: 50px auto;
-  font-size: 0.8rem;
+  font-size: 0.85rem;
   text-align: center;
 }
 
 td,
 th {
-  border-top: 1px solid $primary-gray;
   text-align: left;
-  padding: 20px 10px;
+  color: $dark-gray;
+  padding: 10px 0;
+}
+
+td {
+    &:first-child {
+        padding-left: 10px;
+    }
 }
 
 tr:nth-child(even) {
   background-color: $primary-gray;
+}
+
+.action-column {
+  display: flex;
+  justify-content: space-evenly;
 }
 </style>
