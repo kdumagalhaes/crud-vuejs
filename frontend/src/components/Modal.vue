68f22fb8c19a612f42fbb326e35862f83b5c3eb5
Carlos Pereira <ce.magalhaespereira@gmail.com>
2021-11-01 18:32:20 -0300

    Add close button

---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="showButton" class="modal">
+    <button @click="closeModal" class="close">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  data() {
+    return {
+      showButton: true,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.showButton = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  width: 300px;
+  height: 100vh;
+  background-color: $primary-gray;
+  position: absolute;
+  right: 0;
+  top: 0;
+  box-shadow: -11px 0px 15px -6px rgba(0, 0, 0, 0.35);
+}
+
+.close {
+  border-radius: 3px;
+  border: none;
+  color: $light-gray;
+  font-weight: $bold;
+  padding: 5px;
+  margin: 10px;
+  background-color: $error-red;
+}
+</style>
